c2375fdb288caa8f861f1fbc86d0cfcbba9746ba
Bosshentai <baptistamhernani@gmail.com>
2021-01-11 22:43:30 -0100

    ainda falta 3 i 4

--- ficha04/ex01.hs
+++ ficha04/ex01.hs
@@ -2,7 +2,7 @@ lista :: [a] -> [a]
 lista [] = []
 lista (xs) = take (length xs - 1) xs
 lista [] = []
---lista [_:_]!!2 = [_:_]
+
 --lista xs =  lista (teste)
 --            where
 --                teste = take (length xs -1) xs
