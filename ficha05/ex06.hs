233efe03e0a69ccd7ec1e45e7a60e7b990d5af48
bosshentai <baptistamhernani@gmail.com>
2021-01-23 15:04:42 -0100

    Trabalho completo

--- ficha05/ex06.hs
+++ ficha05/ex06.hs
@@ -2,10 +2,26 @@
 -- argumento  numero
 -- char
 -- lista
---teste = "bolo"
+teste = "bolo"
 
 --remover:: Int -> Char -> [a] -> [a]
 --remover loc letra (x:xs) = [x]
 
 
 --resultado = remover 2 'a' teste
+
+
+removeAt i xs = ys ++ zs
+    where
+        increment = i + 1;
+        (ys,_:zs) = splitAt increment xs
+
+
+remover i f xs = ys ++ [f] ++ zs
+        where
+            increment = i + 1;
+            (ys,_:zs) = splitAt increment xs
+
+resultado = removeAt 2 teste
+
+resultado2 = remover 2 'a' teste
