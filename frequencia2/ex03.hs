fa4c71f69e52716a6c90d9ca07554273fbff3f4b
bosshentai <baptistamhernani@gmail.com>
2021-01-27 10:19:17 -0100

    feito completo

--- frequencia2/ex03.hs
+++ frequencia2/ex03.hs
@@ -2,20 +2,15 @@ teste = [0..4]
 numero1 = 10
 numero2= 100
 
---altMap:: (a -> b) -> (a-> b) -> [a] -> [b]
---altMap (valor1,valor2) xs = ys ++ zs
---                        where
----                        (ys,_:zs) = splitAt (length xs) xs
 
---resultado = altMAp  numero1 numero2
---
+reste (valor1,valor2) n | even n = n +  valor1
+         | otherwise = n + valor2
 
+altMap (valor1,valor2) lista = map ( reste (valor1,valor2) ) lista
 
-
-altMap (valor1,valor2) lista = [x | x <- lista, even x == True  ] 
-    --map (+valor1) (filter (even) lista) ++  map (+valor2) (filter (odd) lista)
 
 resultado = altMap(numero1,numero2) teste
+resultado2 = altMap2(numero1,numero2) teste
 --3.2
 teste2 = [("hernani",12),("lomba",3),("tewtew",12),("asda",10)]
 
